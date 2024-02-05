8be9954ce1c9482568eec40432a23bba90d3b3d8
gibo <gibo.gaf@gmail.com>
2024-02-05 09:34:09 +0100

    create test for triangle_area() and add devtools::test()

--- 00_check.R
+++ 00_check.R
@@ -3,6 +3,7 @@ library(usethis)
 # use_gpl3_license()
 # use_package_doc()
 # use_github_action(name = "check-standard")
+# use_test("triangle_area")
 
 library(devtools)
 
@@ -16,4 +17,6 @@ check()
 install()
 
 build()
+
+test()
 
