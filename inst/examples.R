1ee02038bfb52a78322ec646f76bff7c26d7f52f
gibo <sandro.burri@ti.ch>
2024-02-22 16:30:43 +0100

    add 3 examples of triangle_area()

--- inst/examples.R
+++ inst/examples.R
@@ -7,3 +7,10 @@ ls("package:testrepo")
 
 triangle_area(base = 10, height = 20)
 
+triangle_area(base = 1:10, height = 5)
+
+triangle_area(base = 1:10, height = 11:20)
+
+triangle_area(base = 5, height = 11:20)
+
+
